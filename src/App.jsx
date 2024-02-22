e11ccf6be2e23d9bbab0f517198c26ab0c865bf7
Ali Hager <ahager2013@gmail.com>
2024-02-22 15:58:09 -0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import RandomPic from "./components/RandomPic";
 import { useEffect, useState } from "react";
 import SelectBreed from "./components/SelectBreed"
 import Gallery from "./components/Gallery";
+import { Stack } from "@mui/material";
+import { Button } from "@mui/material";
 
 function App() {
   // create a piece of state to track the cat images then we can change the randomPic imgUrl to be {catImgUrl - make sure to import state at the top}
@@ -11,6 +13,7 @@ function App() {
   const [selectedBreed, setSelectedBreed] = useState("random")
   // 4. add a "favorite cats" list, [] because we want an array
   const [favorites, setFavorites] = useState([])
+  const [visibleTab, setVisibleTab] = useState("random")
 
   // create useEffect to fetch the image from the API and return it into state[], define async function line under this
   // 1. fetch a random cat pic from the API and display it
@@ -52,6 +55,10 @@ console.log(favorites)
 
   return (
     <>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" onClick={() => setVisibleTab('random')}>Random</Button>
+        <Button variant="contained" onClick={()=> setVisibleTab('favorites')}>Favorites</Button>
+      </Stack>
       {/* 3. get a random cat pic by breed */}
       {/* make an input that selects a specific cat break, record that cat breed in the App component state, if we are searching for a specific breed, apply that criteria to the fetch request */}
       {/* then fetch the breeds list and update the input to use the list of available breeds */}
@@ -61,9 +68,9 @@ console.log(favorites)
 
       {/* randomPic component should accept an image URL prop and display that image */}
       {/* give it an imageUrl prop */}
-      <RandomPic imgUrl={catImgUrl} refetchFunction={updateImage} addToFavorites={addToFavorites}/>
+      {visibleTab === 'random' && <RandomPic imgUrl={catImgUrl} refetchFunction={updateImage} addToFavorites={addToFavorites}/>}
       <br></br>
-      <Gallery favoritesArray={favorites}/>
+      {visibleTab === 'favorites' && <Gallery favoritesArray={favorites}/>}
     </>
   );
 }
